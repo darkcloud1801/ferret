31a705ef3ea45d1a9f0d7b0347420a2cb047ea52
Rubin Damian <rubinm@eurekasoft.com>
2024-06-27 12:05:25 -0500

    Enable an ES cluster and configure fscrawler for it

--- docker/fscrawler/config/run_fscrawler.sh
+++ docker/fscrawler/config/run_fscrawler.sh
@@ -2,9 +2,17 @@
 echo "Starting FSCrawler with the following configurations:"
 ls -la /root/.fscrawler/
 
+echo $JAVA_HOME
+
+keytool -delete -alias elastic_security -keystore "$JAVA_HOME\lib\security\cacerts" -storepass changeit -noprompt || true
+keytool -import -alias elastic_security -keystore "$JAVA_HOME\lib\security\cacerts" -file /usr/share/fscrawler/certs/ca/ca.crt -storepass changeit -noprompt
+
+
 for config in /root/.fscrawler/*; do
   if [ -d "$config" ]; then
     echo "Running FSCrawler for config: $(basename "$config")"
     fscrawler --config_dir /root/.fscrawler --loop 1 $(basename "$config") &
   fi
 done
+
+wait
