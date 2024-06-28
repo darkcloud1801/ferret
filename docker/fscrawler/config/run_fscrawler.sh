d12c2d3ca96eec18730fdab89bc3fcc84491712d
Rubin Damian <rubinm@eurekasoft.com>
2024-06-27 21:03:21 -0500

    got it working

--- docker/fscrawler/config/run_fscrawler.sh
+++ docker/fscrawler/config/run_fscrawler.sh
@@ -4,7 +4,7 @@ ls -la /root/.fscrawler/
 
 echo $JAVA_HOME
 
-keytool -delete -alias elastic_security -keystore "$JAVA_HOME\lib\security\cacerts" -storepass changeit || true
+keytool -delete -alias elastic_security -keystore "$JAVA_HOME\lib\security\cacerts" -storepass changeit -noprompt || true
 keytool -import -alias elastic_security -keystore "$JAVA_HOME\lib\security\cacerts" -file /usr/share/fscrawler/certs/ca/ca.crt -storepass changeit -noprompt
 
 
@@ -14,3 +14,5 @@ for config in /root/.fscrawler/*; do
     fscrawler --config_dir /root/.fscrawler --loop 1 $(basename "$config") &
   fi
 done
+
+wait
